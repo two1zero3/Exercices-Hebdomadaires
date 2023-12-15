0576dedd5a9eadf0a2c998d6189164df3f76f367
two1zero3 <74436100+two1zero3@users.noreply.github.com>
2023-12-15 04:59:33 +0100

    EX 5

---
+++ Exercice V/dist/build.js
@@ -0,0 +1,275 @@
+"use strict";
+// la définition de la classe Circle c'est comme définir une function mais sans les parenthèses
+// la fonction par défaul est le constructor
+// on peut passer des paramètres au constructor
+// dans une class on n'écrit pas "function" pour TOUTES les fonctions
+// une variable globale de class s'écrit avec "this."
+class Circle {
+    constructor(x, y, rayon, context, index) {
+        this.x = x;
+        this.y = y;
+        this.origin = { x: x, y: y };
+        this.target = { x: x, y: y };
+        this.index = index;
+        this.speed = 1;
+        this.uniteDeTemps = 0;
+        this.uniteDeTemps1 = 0;
+        this.rayon = rayon;
+        this.rayonOrigin = { rayon: rayon };
+        this.rayonFinal = { rayon: rayon };
+        this.context = context;
+        // on initialise une couleur au bol
+        this.color = "white";
+        this.rotation = 0;
+        this.angle = 0;
+    }
+    changeColor(r, g, b) {
+        // on affect une couleur aléatoire
+        this.color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+        // this.color = `rgb(${r},${g},${b})`;
+    }
+    changeRadius(pourcentage) {
+        this.rayon = this.rayonOrigin.rayon * pourcentage;
+    }
+    isInMe(mouseX, mouseY) {
+        // on calcule la distance entre la souris et le centre
+        let d = this.dist(mouseX, mouseY, this.x, this.y);
+        // on compare cette distance au rayon
+        if (d < this.rayon) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    shaderColor() {
+        let fragCoord = { x: this.x, y: this.y };
+        let iResolution = { x: largeur, y: hauteur };
+        let uv = {
+            x: fragCoord.x / iResolution.x,
+            y: fragCoord.y / iResolution.y,
+        };
+        let uv0 = uv;
+        uv0.x = uv0.x * 1.0 - .5;
+        uv0.y = uv0.y * 1.0 - .5;
+        uv = { x: uv.x * 2.0 - 1.0, y: uv.y * 2.0 - 1.0 };
+        uv.x = (uv.x * iResolution.x) / iResolution.y;
+        let col;
+        let d;
+        let finalColor = { r: 0, g: 0, b: 0, a: 1.0 };
+        for (let i = 0; i < 2; i++) {
+            uv.x *= 1.25;
+            uv.y *= 1.25;
+            uv.x = uv.x - Math.floor(uv.x);
+            uv.y = uv.y - Math.floor(uv.y);
+            uv.x -= 0.5;
+            uv.y -= 0.5;
+            d = Math.sqrt(uv.x * uv.x + uv.y * uv.y);
+            // let ll = this.lengthGLSL(uv0.x, uv0.y);
+            let ll = this.rayon;
+            d = Math.floor(d + ll);
+            col = this.palette(ll + frameCount * 0.01);
+            d = Math.sin(d * 10.0 + frameCount * 0.01) * 1;
+            d = Math.abs(d);
+            d = 0.2 / d;
+            finalColor.r += col.r * d;
+            finalColor.g += col.g * d;
+            finalColor.b += col.b * d;
+        }
+        let color0 = { r: finalColor.r, g: finalColor.g, b: finalColor.b, a: 1.0 };
+        this.color = `rgb(${color0.r * 255},${color0.g * 255},${color0.b * 255})`;
+    }
+    lengthGLSL(x, y) {
+        return Math.sqrt(x * x + y * y);
+    }
+    palette(t) {
+        let a = { x: 0.5, y: 0.5, z: 0.5 };
+        let b = { x: 0.5, y: 0.5, z: 0.5 };
+        let c = { x: 1.0, y: 1.0, z: 1.0 };
+        let d = { x: 0.263, y: 0.416, z: 0.557 };
+        let first = a.x + b.x * Math.cos(6.28318 * (c.x * t + d.x));
+        let second = a.y + b.y * Math.cos(6.28318 * (c.y * t + d.y));
+        let third = a.z + b.z * Math.cos(6.28318 * (c.z * t + d.z));
+        return { r: first, g: second, b: third };
+    }
+    draw() {
+        this.move();
+        this.shaderColor();
+        //pour préparer la rotation
+        this.context.save();
+        //on translate le contexte au centre du cercle
+        this.context.translate(this.x, this.y);
+        //on fait la rotation
+        this.context.rotate(this.rotation);
+        //on dessine le cercle
+        this.context.fillStyle = this.color;
+        this.context.beginPath();
+        this.context.arc(0, 0, this.rayon, 0, 2 * Math.PI, true);
+        //this.context.rect(0, 0, this.rayon * 2, this.rayon * 2);
+        this.context.fill();
+        this.context.closePath();
+        this.context.restore();
+    }
+    dist(x1, y1, x2, y2) {
+        // calcule la distance entre deux points
+        // pythagore power
+        let d = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        return d;
+    }
+    definirDestination(x, y) {
+        this.target = { x: x, y: y };
+        this.uniteDeTemps = 0;
+    }
+    definirRayonAleatoire() {
+        this.rayonFinal.rayon = Math.random() * 200 + 50;
+        this.uniteDeTemps1 = 0;
+    }
+    move() {
+        this.angle++;
+        //   this.x = this.origin.x + Math.cos(this.angle * (Math.PI / 180)) * 100;
+        //   this.y = this.origin.y + Math.sin(this.angle * (Math.PI / 180)) * 100;
+    }
+    rapetisser() {
+        let differenceRayon2 = this.rayonFinal.rayon - this.rayon;
+        // console.log(differenceRayon2);
+        if (Math.abs(differenceRayon2) < 0.01) {
+            this.rayonOrigin = { rayon: this.rayonFinal.rayon };
+            return;
+        }
+        const easing = Easing.elasticOut(this.uniteDeTemps1);
+        this.uniteDeTemps1 += 0.01;
+        let differenceRayon = this.rayonFinal.rayon - this.rayonOrigin.rayon;
+        this.rayon = this.rayonOrigin.rayon + differenceRayon * easing;
+    }
+}
+// largeur totale de l'écran
+var width = window.innerWidth;
+// hauteur totale de l'écran
+var height = window.innerHeight;
+// contexte 2D
+var context;
+// image fixed
+var image;
+var frameCount = 0;
+// largeur et hauteur par default de l'image ou de la video
+var largeur = 1000;
+var hauteur = 1000;
+var gridXY = 20;
+var gridX = largeur / gridXY;
+var gridY = hauteur / gridXY;
+// tableau pour stocker la grille de cercles
+var grille = [];
+// variable pour stocker les pixels de l'image video
+var video;
+// une variable pour définir si on utilise la webcam ou l'image fixe
+var webcam = true;
+// fonction pour créer un canvas
+function createCanvas(w, h) {
+    var canvas = document.createElement("canvas");
+    canvas.width = w;
+    canvas.height = h;
+    context = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+}
+function setup() {
+    console.log("setup");
+    createCanvas(width, height);
+    // on active la gestion de la souris
+    document.addEventListener("mousedown", mousePressed);
+    // pour l'exemple avec la webcam , on initialise la caméra
+    if (webcam) {
+        initialiserCamera();
+    }
+    else {
+        // pour l'exemple avec l'image fixe
+        image = new Image();
+        // on attend que l'image soit chargée avant de l'afficher
+        image.onload = () => {
+            //on peut récupèrer la largeur et la hauteur de l'image
+            largeur = image.width;
+            hauteur = image.height;
+        };
+        // on définit la source de l'image
+        image.src = "image/andy.jpg";
+    }
+    let index = 0;
+    //on créé une grille de cercles
+    // pour une grille de 1000x1000
+    for (let j = 0; j < hauteur; j += 20) {
+        for (let i = 0; i < largeur; i += 20) {
+            //get index that is goes up by one for every new circle
+            let circle = new Circle(i, j, 10, context, index);
+            // on affecte un angle incrémenteal à chaque cercle
+            circle.angle = i * 0.2;
+            // on stock le cercle dans le tableau
+            grille.push(circle);
+            index++;
+        }
+    }
+    draw();
+}
+function draw() {
+    // on analyse les pixels de l'image
+    detectPixels();
+    //on efface tout l'écran en noir
+    context.fillStyle = "black";
+    context.fillRect(0, 0, width, height);
+    // on dessine les cercles
+    grille.forEach((circle, i) => {
+        circle.draw();
+        // le mouvment de chaque cercle est géré dans la fonction draw de la class Circle
+    });
+    frameCount++;
+    requestAnimationFrame(draw);
+}
+function detectPixels() {
+    // on prépare une variable pour stocker les pixels
+    let pixels;
+    // on dessine l'image dans le contexte
+    // attention si on veut l'image static, il faut remplacer video par image
+    if (webcam) {
+        context.drawImage(video, 0, 0);
+    }
+    else {
+        context.drawImage(image, 0, 0);
+    }
+    // on récupère les pixels de l'image
+    pixels = context.getImageData(0, 0, largeur, hauteur);
+    // on parcours tous les cercles
+    grille.forEach((circle, i) => {
+        //récupérer la couleur du pixel par l'index
+        let index = (circle.origin.y * largeur + circle.origin.x) * 4;
+        // on récupère les valeurs de rouge, vert et bleu
+        let r = pixels.data[index];
+        let g = pixels.data[index + 1];
+        let b = pixels.data[index + 2];
+        // on calcule l'intensité de la couleur
+        let intensity = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+        // circle.changeColor(r, g, b);
+        // on change le rayon du cercle en fonction de l'intensité (pourcentage de 0 à 1)
+        circle.changeRadius(intensity / 255);
+    });
+}
+function initialiserCamera() {
+    video = document.createElement("video");
+    navigator.getMedia =
+        navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
+    navigator.getMedia({
+        video: { width: largeur, height: hauteur },
+        audio: false,
+    }, (stream) => {
+        video.srcObject = stream;
+        video.play();
+    }, (error) => {
+        console.log(error);
+    });
+}
+function mousePressed(e) { }
+window.onload = function () {
+    console.log("on est pret");
+    setup();
+};
+//# sourceMappingURL=build.js.map
---
+++ Exercice V/og/js/script.js
@@ -0,0 +1,136 @@
+// largeur totale de l'écran
+var width = window.innerWidth;
+// hauteur totale de l'écran
+var height = window.innerHeight;
+// contexte 2D
+var context;
+// image fixed
+var image = null;
+// largeur et hauteur par default de l'image ou de la video
+var largeur = 1000;
+var hauteur = 1000;
+// tableau pour stocker la grille de cercles
+var grille = [];
+// variable pour stocker les pixels de l'image video
+var video = null;
+// une variable pour définir si on utilise la webcam ou l'image fixe
+var webcam = true;
+
+// fonction pour créer un canvas
+function createCanvas(w, h) {
+  var canvas = document.createElement("canvas");
+  canvas.width = w;
+  canvas.height = h;
+  context = canvas.getContext("2d");
+  document.body.appendChild(canvas);
+}
+
+function setup() {
+  console.log("setup");
+  createCanvas(width, height);
+  // on active la gestion de la souris
+  document.addEventListener("mousedown", mousePressed);
+
+  // pour l'exemple avec la webcam , on initialise la caméra
+  if (webcam) {
+    initialiserCamera();
+  } else {
+    // pour l'exemple avec l'image fixe
+    image = new Image();
+    // on attend que l'image soit chargée avant de l'afficher
+    image.onload = () => {
+      //on peut récupèrer la largeur et la hauteur de l'image
+      largeur = image.width;
+      hauteur = image.height;
+    };
+    // on définit la source de l'image
+    image.src = "image/andy.jpg";
+  }
+  //on créé une grille de cercles
+  // pour une grille de 1000x1000
+  for (let j = 0; j < hauteur; j += 20) {
+    for (let i = 0; i < largeur; i += 20) {
+      let circle = new Circle(i, j, 10, context);
+      // on affecte un angle incrémenteal à chaque cercle
+      circle.angle = i * 0.2;
+      // on stock le cercle dans le tableau
+      grille.push(circle);
+    }
+  }
+  draw();
+}
+
+function draw() {
+  // on analyse les pixels de l'image
+  detectPixels();
+  //on efface tout l'écran en noir
+  context.fillStyle = "black";
+  context.fillRect(0, 0, width, height);
+  // on dessine les cercles
+  grille.forEach((circle, i) => {
+    circle.draw();
+    // le mouvment de chaque cercle est géré dans la fonction draw de la class Circle
+  });
+
+  requestAnimationFrame(draw);
+}
+
+function detectPixels() {
+  // on prépare une variable pour stocker les pixels
+  let pixels = null;
+  // on dessine l'image dans le contexte
+  // attention si on veut l'image static, il faut remplacer video par image
+  if (webcam) {
+    context.drawImage(video, 0, 0);
+  } else {
+    context.drawImage(image, 0, 0);
+  }
+  // on récupère les pixels de l'image
+  pixels = context.getImageData(0, 0, largeur, hauteur);
+
+  // on parcours tous les cercles
+  grille.forEach((circle, i) => {
+    //récupérer la couleur du pixel par l'index
+    let index = (circle.origin.y * largeur + circle.origin.x) * 4;
+    // on récupère les valeurs de rouge, vert et bleu
+    let r = pixels.data[index];
+    let g = pixels.data[index + 1];
+    let b = pixels.data[index + 2];
+    // on calcule l'intensité de la couleur
+    let intensity = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    // circle.changeColor(r, g, b);
+    // on change le rayon du cercle en fonction de l'intensité (pourcentage de 0 à 1)
+    circle.changeRadius(intensity / 255);
+  });
+}
+
+function initialiserCamera() {
+  video = document.createElement("video");
+  navigator.getMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  navigator.getMedia(
+    {
+      video: { width: largeur, height: hauteur },
+      audio: false,
+    },
+    (stream) => {
+      video.srcObject = stream;
+      video.play();
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+function mousePressed(e) {}
+
+window.onload = function () {
+  console.log("on est pret");
+  setup();
+};
